1a6506016a751ef76e6e8db903ccc1d87950a920
William Pierce Wierschem <wwwierschem@icloud.com>
2023-11-02 16:07:56 -0400

    Added update user name function, got it working

--- src/main/java/com/backend/stock_research_main/getUserData.java
+++ src/main/java/com/backend/stock_research_main/getUserData.java
@@ -11,6 +11,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -38,6 +40,22 @@ public class getUserData {
             }
         return ResponseEntity.ok(username);
 
+        } catch (Exception e) {
+            return ResponseEntity.ok("Error");
+        }
+    }
+
+    @CrossOrigin(origins = "http://localhost:4200")
+    @PostMapping(path = "/updateUsername/{email}")
+    public ResponseEntity<String> updateUsername(@PathVariable String email, @RequestBody String username) {
+        final DataSource dataSource = createDataSource();
+        try {
+            final Connection conn = dataSource.getConnection();
+            PreparedStatement sql = conn.prepareStatement("UPDATE users SET username = ? WHERE email = ?");
+            sql.setString(1, username);
+            sql.setString(2, email);
+            sql.executeUpdate();
+            return ResponseEntity.ok("Username updated");
         } catch (Exception e) {
             return ResponseEntity.ok("Error");
         }
