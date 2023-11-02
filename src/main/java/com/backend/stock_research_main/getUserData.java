0d2acafc5249408beecafd4959afb74f7867c684
William Pierce Wierschem <wwwierschem@icloud.com>
2023-11-01 20:19:08 -0400

    Successfully sent username to frontend using plain text, need to change it to response entity to prevent CORS error

--- src/main/java/com/backend/stock_research_main/getUserData.java
+++ src/main/java/com/backend/stock_research_main/getUserData.java
@@ -7,11 +7,10 @@ import java.sql.ResultSet;
 import javax.sql.DataSource;
 
 import org.postgresql.ds.PGSimpleDataSource;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -24,8 +23,8 @@ public class getUserData {
     }
 
     @CrossOrigin(origins = "http://localhost:4200")
-    @GetMapping(path = "/getUserData", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> getAllUserData(@RequestBody String email) {
+    @GetMapping(path = "/getUserData/{email}")
+    public ResponseEntity<String> getAllUserData(@PathVariable String email) {
         final DataSource dataSource = createDataSource();
         String username = "";
 
@@ -37,9 +36,8 @@ public class getUserData {
             while (returnedRows.next()) {
                 username = returnedRows.getString("username");
             }
-            
-        
         return ResponseEntity.ok(username);
+
         } catch (Exception e) {
             return ResponseEntity.ok("Error");
         }
